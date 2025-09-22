9ae545d0bd530d641526ad4e1463f1323239d1e5
Amruth S K <amruthsk25@gmail.com>
2025-09-22 19:01:26 +0530

    learn(node): created mongo db

---
+++ learnings/node/S1/1.2_database.js
@@ -0,0 +1,57 @@
+const { MongoClient } = require("mongodb");
+
+
+const url =
+  "mongodb+srv://Node_db_user:CLnpRVFJUi966gvs@nodecluster.ztj1nyo.mongodb.net/Helloworld?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(url, {
+    tlsAllowInvalidCertificates: true,
+  });
+
+
+// [our db name]
+const dbName = "Helloworld";
+
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+
+  const collection = db.collection("User");
+
+
+
+  //create new user
+
+  const data = {
+    firstname: "Amruth",
+    lastname: "S K",
+    city: "Hosur",
+    country: "India",
+  };
+  // //insert data- creating new documents
+  // const insertResult = await collection.insertMany([data]);
+  // console.log('Inserted documents =>', insertResult);
+
+  //update
+
+  const updateResult = await collection.updateOne(
+    { firstname: "Amruth" },
+    { $set: { city: "Banglore" } }
+  );
+  console.log("Updated documents =>", updateResult);
+
+  //read - find documents
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
