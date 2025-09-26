88c3e0032cf1ee55b3ee79b8c9adb29375bf3863
Amruth S K <amruthsk25@gmail.com>
2025-09-26 15:21:11 +0530

    feat(auth): implement dummy user registration API endpoint

--- projects/devconnect/src/app.js
+++ projects/devconnect/src/app.js
@@ -1,67 +1,38 @@
-console.log("starting a new project");
+console.log("starting devconnect project");
 const express = require("express");
 const app = express();
-const {adminAuth, userAuth} = require("./middlewares/auth.js")
+const connectDB = require("./config/database.js");
+const User = require("./models/user.js")
 
-
-app.use("/admin", adminAuth);
-
-
-
-
-app.get("/admin/getdata",(req,res) => {
-  res.send("data send")
-})
-
-app.get("/connect/getdata", (req, res) => {
-  try{
-    //(Throw Error) @ [Sync Execution Path]
-    throw new Error("Synchronous Boom!");
-    res.send("data send");
-  }
-  catch(err){
-
-    res.status(500).send("server went wrong");
-  }
-  
+app.post("/signup", async (req ,res) => {
+  const user = new User({
+    firstName:"Krish"  ,
+    lastName: "v" ,
+    emailId:  "krish@1234",
+    password: "45698",
 });
+try{
+  //throw new Error("Async error!");
+   await user.save();
+    res.send("user adder succesfully");
+}
+catch(err){
+  res.status(500).send({ message: "Error creating user", error: err.message });
+}
 
-
-//async errorhandling
-app.get("/user/getdata",userAuth, (req, res,next) => {
-   setTimeout(() => {
-      try {
-          //throw new Error("Async error!");
-          res.send("user gets data")
-      } catch(err) {
-          // (Propagate Error) / [next(err)]
-          next(err); 
-      }
-  }, 0);
 });
+//success|error
+connectDB()
+  .then(() => {
+    console.log("database connected");
+
+    // Start server
+    app.listen(9999, () => {
+      console.log("Server is successfully listening on port 9999");
+    });
+  })
+  .catch((err) => {
+    console.log("database not connected");
+  });
 
 
-
-app.get("/user/login", (req, res) => {
-  res.send("Login");
-});
-
-app.get("/user/profile",userAuth, (req, res) => {
-  res.send("profile data send");
-});
-
-
-app.use("/",(err,req,res,next) => {
-  if(err){
-      res.status(500).send("something went wrong");
-
-  }
-})
-
-
-
-
-// Start server
-app.listen(9999, () => {
-  console.log("Server is successfully listening on port 9999");
-});
