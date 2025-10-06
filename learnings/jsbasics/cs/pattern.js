5b968f46d4377bd1179c4cc506369be2fb18f70a
Amruth S K <amruthsk25@gmail.com>
2025-10-06 20:51:41 +0530

    learn(js): review

--- learnings/jsbasics/cs/pattern.js
+++ learnings/jsbasics/cs/pattern.js
@@ -5,10 +5,25 @@ let y = x++;
 let z = ++x;
 
 
-console.log(x);
-console.log(y);
-console.log(z);
+console.log(`x is:` ,x );
+console.log(`y is:`, y);
+console.log(`z is:`, z);
 
 let n = ( x==y ) ? z++ : ++z;
 
 console.log(n);
+console.log("--------------------------------------------")
+//2
+const f = new Boolean(false);
+
+if(f){
+    console.log(1);
+}
+else{
+    console.log(2);
+}
+
+console.log(typeof f);
+
+
+console.log("--------------------------------------------");
