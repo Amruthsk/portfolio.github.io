343496b80075b24a98797ca83a15e250f123595a
Amruth S K <amruthsk25@gmail.com>
2025-10-07 06:55:00 +0530

    learn(js) = basic patterns

--- learnings/jsbasics/cs/pattern.js
+++ learnings/jsbasics/cs/pattern.js
@@ -27,3 +27,35 @@ console.log(typeof f);
 
 
 console.log("--------------------------------------------");
+
+//3 
+const obj = {x:1,y:2};
+let {x:a, y:b} = obj;
+a = 2;
+console.log(obj.x, obj.y); //how 1,2  why not 2,2
+
+console.log("--------------------------------------------");
+
+//4
+let elf = "ESTEL"
+
+function lapland(parmas){
+    console.log(elf);
+    let elf = "Arwen";
+}
+
+//lapland();//reference error
+// why not Arwen within scope or ESTEL outside the function 
+//why error
+
+console.log("--------------------------------------------");
+
+//5
+const obje = {};
+obje.value = undefined;
+console.log(Object.hasOwn(obje, "value"));
+
+
+console.log("--------------------------------------------");
+
+console.log("--------------------------------------------");
