b60f225558cfdfe1b58fd72524f262da65f1d40e
Amruth S K <amruthsk25@gmail.com>
2025-10-12 13:36:54 +0530

    feat(connections): create API to get all of a user's accepted connections

--- projects/devconnect/src/routes/user.js
+++ projects/devconnect/src/routes/user.js
@@ -1,41 +1,82 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
-const ConnectionRequest = require("../models/connectionrequest.js")
+const ConnectionRequest = require("../models/connectionrequest.js");
 const User = require("../models/user.js");
 
+const userSafeData = "firstName lastName photoUrl about skills";
 
-
-userRouter.get("/user/requests/received", userAuth, async(req, res) => {
-try {
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
     const loggedInUserId = req.user._id;
     const requests = await ConnectionRequest.find({
       toUserId: loggedInUserId,
       status: "INTERESTED",
     }).populate(
       "fromUserId", // The path to populate.
-      "firstName lastName photoUrl about skills" // string of fields to include.
+      userSafeData // string of fields to include.
     );
-     console.log("Found requests:", requests);
-      if (requests.length === 0) {
-        return res.status(200).json({
-          message: "You have no pending connection requests.",
-          data: [],
-        });
-      }
-
-      res.json({
-        message: "Data Fetched Successfully",
-        data: requests,
+    console.log("Found requests:", requests);
+    if (requests.length === 0) {
+      return res.status(200).json({
+        message: "You have no pending connection requests.",
+        data: [],
       });
+    }
 
-}
-catch(err){
+    res.json({
+      message: "Data Fetched Successfully",
+      data: requests,
+    });
+  } catch (err) {
     res.status(500).json({
       message: "Server error while fetching requests.",
-      error: err.message,});
-}
-
-
+      error: err.message,
+    });
+  }
 });
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    //resource
+    const loggedInUserId = req.user._id;
+
+    //find
+    const connections = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+      status: "ACCEPTED",
+    })
+      .populate("fromUserId", userSafeData)
+      .populate("toUserId", userSafeData);
+
+    console.log("Found connections:", connections);
+
+    if (connections.length === 0) {
+      return res.status(200).json({
+        message: "You have no accepted connections.",
+        data: [],
+      });
+    }
+
+    const connectedUsers = connections.map((connection) => {
+      if (connection.fromUserId._id.equals(loggedInUserId)) {
+        return connection.toUserId;
+      } else {
+        return connection.fromUserId;
+      }
+    });
+
+    res.status(200).json({
+      message: "Connections fetched successfully.",
+      data: connectedUsers,
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error while fetching connections.",
+      error: err.message,
+    });
+  }
+});
+
 module.exports = userRouter;
