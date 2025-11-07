618a19b8093854beb25e87a4bb60d574b7a15f38
Amruth S K <amruthsk25@gmail.com>
2025-11-07 17:41:37 +0530

    feat(client): build the user interface for the login page

--- projects/devconnect/client/src/Login.jsx
+++ projects/devconnect/client/src/Login.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center my-10">
+      <div className="card w-96 bg-base-300 card-md shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+
+          <div className="py-4">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+
+          <div className="py-4">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login
