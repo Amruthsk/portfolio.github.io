b13ef959411f85e12313993f3c8b6c11395286af
Amruth S K <amruthsk25@gmail.com>
2025-11-08 18:37:23 +0530

    refactor(auth): add state management to the Login component

--- projects/devconnect/client/src/Login.jsx
+++ projects/devconnect/client/src/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Login = () => {
+    const[emailId, setEmailId] = useState("");
+    const[password, setPassword] = useState("")
   return (
     <div className="flex justify-center my-10">
       <div className="card w-96 bg-base-300 card-md shadow-xl ">
@@ -16,6 +19,8 @@ const Login = () => {
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
               />
             </div>
           </div>
@@ -29,6 +34,8 @@ const Login = () => {
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
