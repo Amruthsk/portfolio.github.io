703d8bc874a22c7578a996cb0b326591bb51c3ea
Amruth S K <amruthsk25@gmail.com>
2025-11-08 18:51:24 +0530

    feat(auth): add API client and form submission handler for login

--- projects/devconnect/client/src/Login.jsx
+++ projects/devconnect/client/src/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-    const[emailId, setEmailId] = useState("");
-    const[password, setPassword] = useState("")
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5173/login", {
+        emailId,
+        password,
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="flex justify-center my-10">
       <div className="card w-96 bg-base-300 card-md shadow-xl ">
@@ -41,12 +54,12 @@ const Login = () => {
           </div>
 
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
