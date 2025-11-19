92da41ed79d14d621adcda9fc83a3c208c71cc36
Amruth S K <amruthsk25@gmail.com>
2025-11-19 20:33:18 +0530

    refactor(state): add reducer to remove a request from the store

--- projects/devconnect/client/src/utils/requestSlice.js
+++ projects/devconnect/client/src/utils/requestSlice.js
@@ -8,8 +8,11 @@ const requestSlice = createSlice({
     addRequests: (state, action) => {
       return action.payload;
     },
-},
+    removeRequest: (state, action) => {
+      return state.filter((request) => request._id !== action.payload);
+    },
+  },
 });
 
-export const { addRequests } = requestSlice.actions;
+export const { addRequests, removeRequest } = requestSlice.actions;
 export default requestSlice.reducer;
