b431cd61ef85afbc8d6febba79468ecb9bc8e7d2
Amruth S K <amruthsk25@gmail.com>
2025-11-20 16:46:58 +0530

    feat(auth): implement client-side user signup flow

--- projects/devconnect/client/src/components/Login.jsx
+++ projects/devconnect/client/src/components/Login.jsx
@@ -12,11 +12,14 @@ const Login = () => {
   const navigate = useNavigate();
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
-  const [error,setError] = useState("");
+  const [error, setError] = useState("");
+  const [isLoginForm, setisLoginForm] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleLogin = async () => {
     try {
-      setError(""); 
+      setError("");
       const res = await axios.post(
         BASE_URL + "/login",
         {
@@ -31,16 +34,70 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (err) {
-       setError(
-         err?.response?.data || "Something went wrong. Please try again."
-       );
+      setError(
+        err?.response?.data || "Something went wrong. Please try again."
+      );
     }
   };
+   const handleSignup = async () => {
+     try {
+       setError("");
+       const res = await axios.post(
+         BASE_URL + "/signup",
+         {firstName,
+          lastName,
+           emailId,
+           password,
+         },
+         {
+           withCredentials: true,
+         }
+       );
+       console.log("Signup successful, user created:", res.data);
+
+       dispatch(addUser(res.data.data));
+       return navigate("/profile");
+     } catch (err) {
+       setError(err?.response?.data || "Signup failed. Please try again.");
+     }
+   };
   return (
     <div className="flex justify-center my-10">
       <div className="card w-96 bg-base-300 card-md shadow-xl ">
         <div className="card-body">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title">{isLoginForm ? "Login" : "Sign Up"}</h2>
+
+          {!isLoginForm && (
+            <>
+              {/* First Name Input */}
+              <div className="py-4">
+                <label className="label">
+                  <span className="label-text">First Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="input input-bordered"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+
+              {/* Last Name Input */}
+              <div className="py-4">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className="input input-bordered"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+            </>
+          )}
 
           <div className="py-4">
             <div className="form-control">
@@ -72,15 +129,30 @@ const Login = () => {
             </div>
           </div>
 
-          <div className="card-actions justify-center">
+          <div className="card-actions flex flex-col items-center space-y-3">
             {error && (
-              <p className="text-red-500 text-xs bold mt-2">{error}</p>
+              <p className="text-red-500 text-sm font-semibold mt-2 text-center">
+                {error}
+              </p>
             )}
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+
+            <button
+              className="btn btn-primary w-full py-2 text-lg font-semibold"
+              onClick={isLoginForm ? handleLogin : handleSignup}
+            >
+              {isLoginForm ? "Login" : "Create Account"}
             </button>
           </div>
         </div>
+
+        <p
+          onClick={() => setisLoginForm((value) => !value)}
+          className="text-center text-sm  mb-4 cursor-pointer hover:underline text-blue-600"
+        >
+          {isLoginForm
+            ? "New here? Create an account"
+            : "Already have an account? Login"}
+        </p>
       </div>
     </div>
   );
