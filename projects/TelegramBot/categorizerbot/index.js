7318432da8964a9219d8e3a55662aef15276d522
Amruth S K <amruthsk25@gmail.com>
2025-11-26 13:17:44 +0530

    feat(core): implement rules for message categorization

--- projects/TelegramBot/categorizerbot/index.js
+++ projects/TelegramBot/categorizerbot/index.js
@@ -1,14 +1,33 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const categorizationRules = require("./rules");
+
+
 
 console.log("Token Status:", process.env.BOT_TOKEN ? "Loaded" : "FAILED/NULL");
+
+
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) =>
+
+bot.start((ctx) => {
   ctx.reply(
-    "Welcome to classifier Bot  Online. Send me some text to categorize"
-  )
-);
+    "Welcome to classifier Bot Online. Send me some text to categorize"
+  );
+});
+
+bot.on("text", (ctx) => {
+  const message = ctx.message.text;
+  let tag = "Uncategorized"; 
+  for (const [key, patterns] of Object.entries(categorizationRules)) {
+    if (patterns.some((pattern) => pattern.test(message))) {
+      tag = key; 
+      break; 
+    }
+  }
+
+  ctx.reply(`[${tag}] Tag assigned to: "${message.substring(0, 30)}..."`);
+});
 
 bot.launch();
-
 console.log("Bot Listener is active and awaiting connection...");
---
+++ projects/TelegramBot/categorizerbot/rules.js
@@ -0,0 +1,7 @@
+const categorizationRules = {
+  Project_Idea: [/idea/i, /new feature/i, /brainstorm/i],
+  Tech_Stack: [/react/i, /node/i, /mongodb/i, /telegraf/i],
+  General_Chat: [/hello/i, /how are you/i, /thanks/i, /haha/i],
+};
+
+module.exports = categorizationRules;
