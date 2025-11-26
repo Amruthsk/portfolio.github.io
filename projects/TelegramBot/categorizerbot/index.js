33d253eb923041a61f78801d93217f67f8e6424c
Amruth S K <amruthsk25@gmail.com>
2025-11-26 12:26:15 +0530

    feat(bot): configure secure instance and establish long-polling connection

--- projects/TelegramBot/categorizerbot/index.js
+++ projects/TelegramBot/categorizerbot/index.js
@@ -1,1 +1,14 @@
+require("dotenv").config();
 const { Telegraf } = require("telegraf");
+
+console.log("Token Status:", process.env.BOT_TOKEN ? "Loaded" : "FAILED/NULL");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) =>
+  ctx.reply(
+    "Welcome to classifier Bot  Online. Send me some text to categorize"
+  )
+);
+
+bot.launch();
+
+console.log("Bot Listener is active and awaiting connection...");
