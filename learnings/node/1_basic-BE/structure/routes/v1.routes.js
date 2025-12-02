310ac8415a1d995a895fb8121fbc7ffbfcbbc094
Amruth S K <amruthsk25@gmail.com>
2025-12-02 15:13:50 +0530

    feat(api/v1): Implement V1 routes for Products and Categories entities

--- learnings/node/1_basic-BE/structure/routes/v1.routes.js
+++ learnings/node/1_basic-BE/structure/routes/v1.routes.js
@@ -1,9 +1,18 @@
 // routes/v1.routes.js
 const express = require("express"); 
 const homePingController = require("../controllers/ping.controller"); 
+const {
+  productGetByIdController,
+  productCreateController,
+  categoryGetByIdController,
+} = require("../controllers/resource.controller");
 
 const v1Router = express.Router(); 
 // Register the controller function to a specific path on the router
 v1Router.get("/ping", homePingController); 
+
+v1Router.get("/products/:id", productGetByIdController);
+v1Router.post("/products", productCreateController);
+v1Router.get("/category/:id", categoryGetByIdController); 
 
 module.exports = v1Router; 
