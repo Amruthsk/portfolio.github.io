2eae7705a6099183f1159669240f6ee766c37d68
Amruth S K <amruthsk25@gmail.com>
2025-12-03 15:27:52 +0530

    feat(server): Enable express.json middleware for body parsing

--- learnings/node/1_basic-BE/structure/src/index.js
+++ learnings/node/1_basic-BE/structure/src/index.js
@@ -8,7 +8,8 @@ const app = express();
 //app.get("/", homePingController);
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
 
 app.use("/api", apiRouter);
 
