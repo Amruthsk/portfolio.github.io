4484a39e360b267d09e749ffe058a6e9cc2c058c
Amruth S K <amruthsk25@gmail.com>
2025-12-03 15:27:14 +0530

    chore: move folders

--- learnings/node/1_basic-BE/structure/src/index.js
+++ learnings/node/1_basic-BE/structure/src/index.js
@@ -1,18 +1,17 @@
 // src/index.js
-const express = require("express"); 
-const { PORT } = require("../config/server.config"); 
-const apiRouter = require("../routes/index.js");
+const express = require("express");
+const { PORT } = require("./config/server.config.js");
+const apiRouter = require("./routes/index.js");
 
-const app = express(); 
+const app = express();
 // Use the controller directly on the base route
-//app.get("/", homePingController); 
+//app.get("/", homePingController);
 
 app.use(express.json());
 
-app.use("/api", apiRouter);  
 
-
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Started server at port: ${PORT}`);  
-}); 
+  console.log(`Started server at port: ${PORT}`);
+});
