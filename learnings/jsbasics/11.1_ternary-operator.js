e1229fb458babf1db9f7a47e88c04c34d25ecab5
Amruth S K <amruthsk25@gmail.com>
2025-12-06 20:35:17 +0530

    learn(js):equality,ternary operator

---
+++ learnings/jsbasics/11.1_ternary-operator.js
@@ -0,0 +1,8 @@
+// 3 expression
+// condition? true:false
+let y = (true) ? 10 : 7
+
+console.log(y)
+
+let z = false ? 10 : 7;
+console.log(z);
--- learnings/jsbasics/7.2_equality.js
+++ learnings/jsbasics/7.2_equality.js
@@ -1,2 +1,8 @@
 console.log( 1 == "string"); //NaN
 console.log(NaN === NaN); //false
+
+
+console.log(typeof "1")
+console.log(typeof 1)
+console.log(typeof false);
+console.log(typeof null);// object  but should be null  
